274a6087f0d0364433d044e6c451ff769065feb3
esilaeva <126511021+esilaeva@users.noreply.github.com>
2023-08-12 21:54:40 +0300

    added changes homework8

--- src/test/java/com/demoqa/tests/RegistrationPageObjectsWithFakerTests.java
+++ src/test/java/com/demoqa/tests/RegistrationPageObjectsWithFakerTests.java
@@ -13,7 +13,6 @@ public class RegistrationPageObjectsWithFakerTests extends TestBase {
 
     @Test
     void successRegistrationFullTest() {
-
         registrationPage
                 .openPage()
                 .setFirstName(firstName)
