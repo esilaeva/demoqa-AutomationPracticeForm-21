2a3f5a1d9998651560ccd0150cc616773314a605
esilaeva <emsilaeva@gmail.com>
2023-08-12 19:17:21 +0300

    added changes homework 8

--- src/test/java/com/demoqa/utils/Testdata.java
+++ src/test/java/com/demoqa/utils/Testdata.java
@@ -3,18 +3,20 @@ package com.demoqa.utils;
 import static com.demoqa.utils.RandomUtils.*;
 
 public class Testdata {
+    public static String[] birthday = getRandomDate(14, 99);
     public static String
             firstName = getRandomFirstName(),
             lastName = getRandomLastName(),
             userEmail = getRandomEmail(),
             userGender = getRandomGender(),
             userNumber = getRandomPhone(),
+            day = birthday[0],
+            month = birthday[1],
+            year = birthday[2],
             subject = getRandomSubject(),
             hobby = getRandomHobbies(),
             fileName = "pic.png",
             streetAddress = getRandomAddress(),
             state = getRandomState(),
             city = getRandomCity(state);
-
-    public static String[] birthday = getRandomDate(14, 99);
 }
